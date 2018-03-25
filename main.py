2db7ff15e71c2559af0f96892edae536d53de583
Abdullah AlAttar <abdullah.alattar@cis.asu.edu.eg>
2018-03-25 23:13:45 +0200

    Spagheti masters

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ class State(Enum):
 state = State.MATCHING
 
 sift = cv2.xfeatures2d.SIFT_create()
-# ret, frame = cap.read()
 
 
 keypoints_database = pickle.load(open("openHand.p", "rb"))
@@ -27,32 +26,33 @@ kpopen, descopen = unpickle_keypoints(keypoints_database)
 
 keypoints_database = pickle.load(open("closeHand.p", "rb"))
 kpclosed, descclosed = unpickle_keypoints(keypoints_database)
+
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FPS, 20)
-# print([i.pt for i in kpopen[:5]])
-# print(descopen[:5])
 
-# fourcc = cv2.VideoWriter_fourcc(*'DIVX')
-# out = cv2.VideoWriter('output.avi', -1, 20.0, (250, 250))
-
+ret, frame = cap.read()
+fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+out = cv2.VideoWriter('output.avi', -1, 20.0, (640, 480))
 
 saved = True
 text = ["Openend Hand", "Closed hand"]
 cnt = 0
-# orb = cv2.ORB_create()
 
-# flann_params = dict(algorithm=6, table_number=6,
-#                     key_size=12, multi_probe_level=1)
 
-# flann = cv2.FlannBasedMatcher(flann_params, {})
 FLANN_INDEX_KDTREE = 0
 index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
 search_params = dict(checks=50)
 
 flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+# create BFMatcher object
+bf = cv2.BFMatcher()
+
+# Match descriptors.
+# matches = bf.match(des1, des2)
 ret, frame = cap.read()
-# x_start, x_end, y_start, y_end = frame.shape[1] - \
-#     roi_offset, frame.shape[1], frame.shape[0] - roi_offset, frame.shape[0]
+
+
 x_start, x_end, y_start, y_end = 200, 350, 200, 350
 rectangles = []
 while True:
@@ -62,6 +62,8 @@ while True:
     roigray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
 
     kp, dest = sift.detectAndCompute(gray_frame, None)
+    # frame = cv2.drawKeypoints(
+    # frame, kp, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS, outImage=None)
 
     c = cv2.waitKey(1)
 
@@ -74,18 +76,26 @@ while True:
         state = State.RECORDING_OPEN
         cnt = 0
         rectangles = []
+        rectangles.append([0, x_start, y_start, x_end, y_end])
     if c == ord('3'):
         state = State.RECORDING_CLOSE
         cnt = 0
+
         rectangles = []
+        rectangles.append([0, x_start, y_start, x_end, y_end])
 
     if state == State.RECORDING_OPEN:
-        if cnt == 24 * 5:
+        if cnt == 20 * 5:
             state = State.MATCHING
         cnt += 1
-        matchesOpen = flann.knnMatch(descopen, dest, k=2)
-        matchesOpen = [match[0] for match in matchesOpen if len(match) == 2
-                       and match[0].distance < match[1].distance * 0.7]
+        print('hello', cnt)
+        # matchesOpen = flann.knnMatch(descopen, dest, k=2)
+        # matchesOpen = [match[0] for match in matchesOpen if len(match) == 2
+        #    and match[0].distance < match[1].distance * 0.7]
+        matchesOpen = bf.match(descopen, dest)
+        # bf.knnMatch()
+        matches = [match for match in matchesOpen]
+        matches = [match for match in matches if match.distance < 300]
 
         if len(matchesOpen) > 1:
 
@@ -120,16 +130,12 @@ while True:
             cv2.putText(img2, str(cnt), (20, 20),
                         cv2.FONT_HERSHEY_SIMPLEX, 1, color=(0, 0, 0))
             cv2.imshow('Frame2', img2)
+            print(img2.shape)
+            out.write(img2)
+            rectangles.append([cnt, mnx, mny, mxx, mxy])
 
-            rectangles.append([mnx, mny, mxx, mxy])
-            # Your Code Goes here
-            # ----------------------------------------------------
-            # only save the the calcaulted box, the ground truth box is constant
-            #  write rectangles to a file
-
-            # ----------------------------------------------------
     if state == State.RECORDING_CLOSE:
-        if cnt == 24 * 5:
+        if cnt == 20 * 5:
             state = State.MATCHING
 
         cnt += 1
@@ -171,35 +177,48 @@ while True:
                         cv2.FONT_HERSHEY_SIMPLEX, 1, color=(0, 0, 0))
             cv2.imshow('Frame2', img2)
 
-            rectangles.append([mnx, mny, mxx, mxy])
-            # Your Code Goes here
-            # ----------------------------------------------------
-            # only save the the calcaulted box, the ground truth box is constant
-            #  write rectangles to a file
+            rectangles.append([cnt, mnx, mny, mxx, mxy])
 
-            # ----------------------------------------------------
     if state == State.MATCHING:
 
-        matchesOpen = flann.knnMatch(descopen, dest, k=2)
-
-        matchesClose = flann.knnMatch(descclosed, dest, k=2)
+        # matchesOpen = flann.knnMatch(descopen, dest, k=2)
+        # matchesClose = flann.knnMatch(descclosed, dest, k=2)
+        matchesOpen = bf.match(descopen, dest)
+        matchesClose = bf.match(descclosed, dest)
+        # print(len(matchesClose), len(matchesOpen))
 
         sumOpen, sumClose = 0, 0
+        # matchesOpen = sorted(matchesOpen, key=lambda x: x.distance)
+        # matchesClose = sorted(matchesClose, key=lambda x: x.distance)
+        i = 0
         for match in matchesOpen:
-            sumOpen += match[0].distance
+            i += 1
+            sumOpen += match.distance
+            # if i == 5:
+            #     break
+        # print(match[0])
+        i = 0
         for match in matchesClose:
-            sumClose += match[0].distance
-
-        # print(sumOpen, sumClose)
+            i += 1
+            sumClose += match.distance
+            # if i == 5:
+            #     break
+            # print()
+        sumOpen /= len(matchesOpen)
+        sumClose /= len(matchesClose)
+        print(sumOpen, sumClose)
         winner = 'open' if sumOpen < sumClose else 'close'
-
+        # print(winner)
         matches = matchesOpen if winner == 'open' else matchesClose
 
-        matches = [match[0] for match in matches if len(match) == 2
-                   and match[0].distance < match[1].distance * 0.7]
-        # print(len(matches))
+        # matches = [match[0] for match in matches if len(match) == 2
+        #    and match[0].distance < match[1].distance * 0.7]
+        # print(matches[0].distance)
+        matches = [match for match in matches if match.distance < 300]
+
+        print(len(matches))
         cv2.imshow('Frame2', frame)
-        if len(matches) > 1:
+        if len(matches) > 5:
             if winner == 'open':
                 src_pts = np.float32(
                     [kpopen[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
@@ -209,17 +228,16 @@ while True:
             dst_pts = np.float32(
                 [kp[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
 
-            # print(src_pts[:5],dst_pts[:5])
-            # print(np.average(src_pts, axis=0).flatten())
             M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
-            # matchesMask = mask.ravel().tolist()
-            mask = mask.ravel() != 0
+
             h, w = roigray.shape
 
+            mask = mask.ravel() != 0
             if mask.sum() < 1:
                 continue
             pts = np.float32(
                 [[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
+
             dst = cv2.perspectiveTransform(pts, M)
             mnx = dst[:, :, 0].min()
             mxx = dst[:, :, 0].max()
@@ -234,13 +252,20 @@ while True:
             cv2.putText(frame, winner, (int(mnx - 5), int(mny - 5)),
                         cv2.FONT_HERSHEY_SIMPLEX, 1, color=(0, 0, 0))
 
-            c1, c2 = np.average(dst_pts, axis=0).flatten()
-            cv2.circle(img2, (c1, c2), 2, (255, 255, 255), 2)
+            # c1, c2 = np.average(dst_pts, axis=0).flatten()
+            # cv2.circle(img2, (c1, c2), 2, (255, 255, 255), 2)
             cv2.imshow('Frame2', img2)
-            # cv2.imshow('abc', roi)
-            # cv2.waitKey(0)
-    # out.write(roi)
+
 
 # out.release()
+f = open('rects.txt', mode='w')
+for rect in rectangles:
+    f.write(str(rect[0]) + " " + str(rect[1]) + " " +
+            str(rect[2]) + " " + str(rect[3]) + " " +
+            str(rect[4]) + '\n')
+    # f.write('/n')
+
+f.close()
 cap.release()
+out.release()
 cv2.destroyAllWindows()
--- main2.py
+++ main2.py
@@ -4,10 +4,10 @@ import numpy as np
 from save_features import pickle_keypoints, unpickle_keypoints
 import pickle
 cap = cv2.VideoCapture(0)
-scaling_factor = 0.5
 roi_offset = 150
-history = 100
 # Create the background subtractor object
+
+
 sift = cv2.xfeatures2d.SIFT_create()
 # ret, frame = cap.read()
 
@@ -19,19 +19,12 @@ class HandFeature:
         self.kp = kp
 
 
-orb = cv2.ORB_create()
+# orb = cv2.ORB_create()
 saved = False
 text = ["Openend Hand", "Closed hand"]
 while True:
     ret, frame = cap.read()
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # Apply the background subtraction model to the input frame
-    # Convert from grayscale to 3-channel RGB
-
-    keypoints, descriptors = [], []
-
-    # keypoints = sift.detect(roi, None)
-    # print(len(kp), len(dest))
 
     x_start, x_end, y_start, y_end = frame.shape[1] - \
         roi_offset, frame.shape[1], frame.shape[0] - roi_offset, frame.shape[0]
@@ -39,24 +32,12 @@ while True:
     roi = frame[y_start:y_end, x_start:x_end, :]
 
     roi_gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-    # blur = cv2.GaussianBlur(roi_gray, (5, 5), 0)
-    # ret, thresh = cv2.threshold(blur, 95, 255, cv2.THRESH_BINARY)
 
     kp, dest = sift.detectAndCompute(roi_gray, None)
-    # roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-    # for keypoint, descriptor in zip(kp, dest):
-    #     x, y = keypoint.pt
-    #     if x_start <= x <= x_end and y_start <= y <= y_end:
-    #         keypoints.append(keypoint)
-    #         descriptors.append(descriptor)
-
-    # print(x_start, x_end, y_start, y_end)
 
     roi = cv2.drawKeypoints(
         roi_gray, kp, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS, outImage=None)
-    # keypoints = orb.detect(thresh, None)
-    # keypoints, descriptors = orb.compute(thresh, keypoints)
-    # cv2.drawKeypoints(roi, kp, roi, color=(186, 85, 211))
+
     cv2.rectangle(frame, (x_start, y_start), (x_end, y_end), (0, 255, 0), 2)
 
     cv2.putText(frame, text[0], (50, 50),
@@ -64,13 +45,11 @@ while True:
 
     cv2.imshow('Frame', frame)
     cv2.imshow('roi', roi)
-    # cv2.imshow('thres', thresh)
-    # cv2.imshow('mask1', mask1)
+
     c = cv2.waitKey(1)
     if c == 27:
         break
     if c == ord('s'):
-        # cv2.imwrite('hand.jpg', roi)
         if not saved:
             openHand = HandFeature(dest, kp)
             cv2.imwrite('open.png', roi)
@@ -81,11 +60,10 @@ while True:
             closeHand = HandFeature(dest, kp)
             cv2.imwrite('close.png', roi)
             break
-    # if c == ord('c'):
-    # print(openHand.dest, openHand.kp)
 
-# print([i.pt for i in openHand.kp[:5]])
-print(openHand.dest[:5])
+
+
+
 pickle.dump(pickle_keypoints(openHand.kp, openHand.dest),
             open("openHand.p", "wb"))
 pickle.dump(pickle_keypoints(closeHand.kp, closeHand.dest),
